541d9d1dbf2cf0a68cf0258b7698b8e7201c8dd3
unknown <191240000908@unisnu.ac.id>
2022-07-14 16:06:12 +0700

    commit 4

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,14 @@
-console.log("Hello abi")
+console.log("hi all")
 const express = require('express')
 const dotenv = require('dotenv').config()
 const port = process.env.PORT || 5000
 
 
+
 const app = express()
 
 
+app.get('/api/goals', (req, res )=>{
+    res.send('Get Goals')
+})
 app.listen(port, () => console.log(`Server started on port ${port}`))
