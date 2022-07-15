83eb6e5bb45784ff15e79eed7b740a20a30d31c2
unknown <191240000908@unisnu.ac.id>
2022-07-15 11:03:28 +0700

    commit 6

--- backend/server.js
+++ backend/server.js
@@ -11,4 +11,5 @@ const app = express()
 app.get('/api/goals', (req, res )=>{
     res.send('Get Goals')
 })
+app.arguments('api/goals',require('.routes/goalRoutes.js'))
 app.listen(port, () => console.log(`Server started on port ${port}`))
