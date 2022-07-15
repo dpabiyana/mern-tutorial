d782a4654b2c3b03a236456a024b37365142dec8
unknown <191240000908@unisnu.ac.id>
2022-07-15 10:59:26 +0700

    commit 5

--- backend/router/router.js
+++ backend/router/router.js
@@ -1,7 +1,7 @@
-const express = require('expre')
+const express = require('express')
 const router = express.Router()
 
-router.get('/'(req, res)=>{
+router.get('/', (req,res)=>{
     res.status(200).json({message:'Get Goals'})
 })
 module.exports = router
