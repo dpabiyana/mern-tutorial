548144149c25eb0f9dc7733c7304e5e4bf1c2afe
unknown <191240000908@unisnu.ac.id>
2022-07-15 14:04:06 +0700

    commit 7

--- backend/router/router.js
+++ backend/router/router.js
@@ -1,7 +1,14 @@
 const express = require('express')
 const router = express.Router()
 
-router.get('/', (req,res)=>{
+router.post('/', (req,res)=>{
     res.status(200).json({message:'Get Goals'})
 })
-module.exports = router
+
+router.put('/:id', (req,res)=>{
+    res.status(200).json({message: 'update Goal ${req.params.id}'})
+})
+
+router.delete('/:id' , (req, res)=>{
+    res.status(200).json({message: 'Delete Goal ${req.params.id}'})
+})
